0b9fc8da4041c834508999630218c6f1685ebad8
Paula2305 <polimoreno1976@gmail.com>
2023-12-23 11:51:33 -0300

    Comprobar formulario, validar email y limpiar alerta

--- js/app.js
+++ js/app.js
@@ -1,39 +1,84 @@
+    const form = {
+        nombre: '',
+        email: '',
+        asunto: '',
+        mensaje: ''
+    }
+
+
     const inputEmail = document.querySelector("#email");
     const inputAsunto = document.querySelector("#asunto");
     const inputMensaje = document.querySelector("#mensaje");
     const inputNombre = document.querySelector("#nombre");
     const formulario = document.querySelector('#formulario');
+    const btnSubmit = document.querySelector('.btn-enviar')
 
-    inputNombre.addEventListener("blur", validar);
-    inputEmail.addEventListener("blur", validar);
-    inputAsunto.addEventListener("blur", validar);
-    inputMensaje.addEventListener("blur", validar);
+    inputNombre.addEventListener("input", validar);
+    inputEmail.addEventListener("input", validar);
+    inputAsunto.addEventListener("input", validar);
+    inputMensaje.addEventListener("input", validar);
 
     function validar(e){
         // console.log(e.target.value);
 
         if(e.target.value.trim() === ""){ 
             mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement);
-        }else{
-            console.log('hay algo');
+            form[e.target.name] = '';
+            comprobarFormulario();
+            return;
         }
+
+        if(e.target.id === 'email' && ! validarEmail(e.target.value)){
+            mostrarAlerta('El email no es válido', e.target.parentElement);
+            form[e.target.name] = '';
+            comprobarFormulario();
+            return;
+        }
+
+        limpiarAlerta(e.target.parentElement);
+
+        form[e.target.name] = e.target.value.trim().toLowerCase();
+
+        comprobarFormulario();
     }
 
     function mostrarAlerta(mensaje, referencia){
-        const alerta = document.querySelector('.tw-bg-orange-600');
-        if(alerta){
-            alerta.remove()
-        }
+
+        limpiarAlerta(referencia);
 
         const error = document.createElement('P');
         error.textContent = mensaje;
-           error.classList.add("tw-bg-orange-600", "tw-text-black", "tw-p-2", "tw-text-center");
+           error.classList.add("bg-orange-600", "text-white", "p-2", "text-center");
 
         referencia.appendChild(error);
-        
 
     }
 
+    function limpiarAlerta(referencia){
+        const alerta = referencia.querySelector('.bg-orange-600');
+        if(alerta){
+            alerta.remove()
+        }
+    }
+
+    function validarEmail(email){
+        const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+        return resultado;
+    }
+
+    function comprobarFormulario(){
+
+        if(Object.values(form).includes('')){
+            btnSubmit.style.opacity = '0.5';
+            btnSubmit.disabled = true;
+            return;
+        }
+
+            btnSubmit.style.opacity = '1';
+            btnSubmit.disabled = false;
+        
+    }
 
 
 
